064d899254d1af86f025716507825fd0ce23e9c0
Hal Helms <hal.helms@gmail.com>
2019-03-02 13:00:12 -0800

    finished 19

--- quiz-19/index.js
+++ quiz-19/index.js
@@ -6,7 +6,7 @@ let gradeAnswers = () => {
 	}
 	
   // add correct class for right answers
-  let answers = [2,1,4,4,2,1,1];
+  let answers = [4,2,1,3,2,1,1];
   answers.forEach(function(element, index){
     document.getElementById(`q${index +1}a${element}`).nextSibling.nextSibling.classList.add('correct');
   });
