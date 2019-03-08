1ab2d24b7b758b7b40eb8ee7c4f3fc0910eef740
Hal Helms <hal.helms@gmail.com>
2019-03-08 11:38:08 -0800

    done through 32

---
+++ quiz-32/index.js
@@ -0,0 +1,15 @@
+let gradeAnswers = () => {
+	// dim all answers
+	let labels = document.getElementsByTagName('label');
+	for (let i=0; i<labels.length; i++) {
+		labels[i].classList.add('incorrect');
+	}
+	
+  // add correct class for right answers
+  let answers = [1,3,4,2,3,2,1];
+  answers.forEach(function(element, index){
+    document.getElementById(`q${index +1}a${element}`).nextSibling.nextSibling.classList.add('correct');
+  });
+};
+
+document.getElementById('submit').onclick = gradeAnswers;
